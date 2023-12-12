8de1a2a8109f34d7936971c0a45bdb33777f75b3
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2023-12-12 16:34:00 +0100

    god

--- SQL/Actividades BDD.sql
+++ SQL/Actividades BDD.sql
@@ -10,7 +10,7 @@ CREATE TABLE DEPARTAMENTO
 
 --Act 2 --------------------------------------------------------------------------------------
 CREATE TABLE EMPLEADO
-(id_empleado NUMBER(6),
+(id_empleado NUMBER(6) ,
  nombre VARCHAR2 (30),
  apellidos VARCHAR2(50),
  numero_departamento NUMBER(6)
@@ -47,3 +47,38 @@ RENAME EMPLEADO2 TO EMPLEADO;
 --Act 8 -----------------------------------------------------------------------------------------
 
 --todo lo comentado
+
+--Restricciones
+--Act 1------------------------
+
+CREATE TABLE EMPLEADO
+(id_empleado NUMBER(6) CONSTRAINT numero_empleado PRIMARY KEY,
+ nombre VARCHAR2 (30),
+ apellidos VARCHAR2(50),
+ numero_departamento NUMBER(6)
+);
+
+--Act 2 -------------------------
+
+CREATE TABLE DEPARTAMENTO 
+(id_departamento NUMBER(6) CONSTRAINT id_departamento PRIMARY KEY,
+ nombre_departamento VARCHAR2(40)
+);
+
+--Act 3 ---------------------------
+
+CREATE TABLE EMPLEADO
+(id_empleado NUMBER(6),
+ nombre VARCHAR2 (30),
+ apellidos VARCHAR2(50),
+ num_departamento NUMBER(6) CONSTRAINT numero_departamento PRIMARY KEY,
+CONSTRAINT EMPLEADO_num_departamento_fk FOREIGN KEY(num_departamento)
+REFERENCES DEPARTAMENTO(id_departamento)
+ON DELETE CASCADE
+);
+
+--Act 4 --------------------------------------
+
+SELECT constraint_name,constraint_type , table_name
+FROM user_constraints
+WHERE table_name = 'EMPLEADO'; 
