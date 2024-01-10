1467441a58d5580d6606660997d26ac3f56fe45f
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-01-10 16:55:47 +0100

    bjoñnj

--- SQL/Ejercicios-SQL01.sql
+++ SQL/Ejercicios-SQL01.sql
@@ -37,3 +37,62 @@ FROM EMPLE
 WHERE OFICIO = (SELECT OFICIO
                 FROM EMPLE
                 WHERE APELLIDO = 'JIMENEZ');
+
+----------------Libreria-----------
+
+--1
+
+SELECT *
+from Libreria
+WHERE EJEMPLARES>=8 AND EJEMPLARES<=15;
+
+--2
+
+SELECT*
+FROM Libreria
+where ESTANTE not between 'B' and 'D';
+
+--3
+
+select *
+from libreria 
+where EJEMPLARES <(select EJEMPLARES from libreria where tema like 'medicina');
+
+--4
+
+SELECT tema
+FROM Libreria
+where EJEMPLARES not between 15 and 20;
+
+----------------------Alumnos ,Asignatura y Notas.---------
+
+--1
+
+select NOMBRE
+from asignaturas, alumnos 
+where nombre like '%O%O%O%' and pobla ='Madrid';
+
+--2
+
+SELECT APENOM , nombre , nota
+FROM alumnos , ASIGNATURAS , Notas
+WHERE (nota>=7 AND nota<=8) AND nombre = 'FOL';
+
+--3
+
+SELECT APENOM , nombre , nota
+FROM alumnos , asignaturas , Notas
+WHERE nombre = 'Madrid' AND nota<5;
+
+--4 
+
+select nombre 
+from asignaturas, notas 
+where nota between 5 and 10;
+
+--5
+
+select apenom 
+from alumnos 
+where dni in (select dni from notas where nota=(select nota from notas where dni = (select dni from alumnos where apenom = 'Díaz Fernández. María')
+and cod = (select cod from asignaturas where nombre = 'FOL')));
